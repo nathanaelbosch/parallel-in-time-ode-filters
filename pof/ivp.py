2f9caba88bad286054adf49052c15641df1d5697
Nathanael Bosch <nathanael.bosch@uni-tuebingen.de>
2022-04-25 16:48:19 +0200

    Visualize the IEKS in a notebook
    Also add the Lotka-Volterra problem

--- pof/ivp.py
+++ pof/ivp.py
@@ -16,3 +16,21 @@ def logistic(t0=0, tmax=10.0, y0=None):
         tmax=tmax,
         y0=y0,
     )
+
+
+def lotkavolterra(t0=0.0, tmax=7, y0=None, p=None):
+
+    y0 = y0 or jnp.array([1.0, 1.0])
+    p = p or jnp.array([1.5, 1.0, 3.0, 1.0])
+
+    @jax.jit
+    def f(_, Y, p=p):
+        a, b, c, d = p
+        return jnp.array([a * Y[0] - b * Y[0] * Y[1], -c * Y[1] + d * Y[0] * Y[1]])
+
+    return tornadox.ivp.InitialValueProblem(
+        f=f,
+        t0=t0,
+        tmax=tmax,
+        y0=y0,
+    )
