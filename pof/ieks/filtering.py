ddaef06cea194135b718c4a21fb47937bd8f521d
Nathanael Bosch <nathanael.bosch@uni-tuebingen.de>
2022-05-05 14:42:11 +0200

    Remove the observation noise R

--- pof/ieks/filtering.py
+++ pof/ieks/filtering.py
@@ -24,26 +24,26 @@ def filtering(
     cholR: jnp.ndarray,
     nominal_trajectory: jnp.ndarray,
 ):
+    assert jnp.all(cholR == 0)
     lin_obs_mod = jax.vmap(linearize, in_axes=[None, 0])(
         observation_model, nominal_trajectory
     )
-    return linear_filtering(x0, transition_model, lin_obs_mod, cholQ, cholR)
+    return linear_noiseless_filtering(x0, transition_model, lin_obs_mod, cholQ)
 
 
-def linear_filtering(
+def linear_noiseless_filtering(
     x0: MVNSqrt,
     transition_model: AffineModel,
     observation_model: AffineModel,
     cholQ: jnp.ndarray,
-    cholR: jnp.ndarray,
 ):
-    """Filter a linear state-space model.
+    """Filter a linear state-space model with exact observations.
 
     The `transition_model` is assumed to be constant over time;
     the `observation_model` is assumed to change each step.
     Filtering is done in parallel via `jax.lax.associative_scan`.
     """
-    elems = get_elements(transition_model, observation_model, cholQ, cholR, x0)
+    elems = get_elements(transition_model, observation_model, cholQ, x0)
 
     _, means, cholcovs, _, _ = jax.lax.associative_scan(
         jax.vmap(sqrt_filtering_operator), elems
@@ -55,26 +55,26 @@ def linear_filtering(
 
 
 @jax.jit
-def get_elements(transition_model, observation_model, cholQ, cholR, x0):
+def get_elements(transition_model, observation_model, cholQ, x0):
     N = observation_model.A.shape[0]
     xs = jax.tree_map(lambda z: append_zeros_along_new_axis(z, N - 1), x0)
-    fn = jax.vmap(_get_elem, in_axes=[None, 0, None, None, 0])
-    return fn(transition_model, observation_model, cholQ, cholR, xs)
+    fn = jax.vmap(_get_elem, in_axes=[None, 0, None, 0])
+    return fn(transition_model, observation_model, cholQ, xs)
 
 
 @jax.jit
-def _get_elem(transition_model, observation_model, cholQ, cholR, xs):
+def _get_elem(transition_model, observation_model, cholQ, xs):
     F, b = transition_model
     H, c = observation_model
 
     ms, Ls = xs
 
-    nx = cholQ.shape[0]
-    ny = cholR.shape[0]
+    nx = b.shape[0]
+    ny = c.shape[0]
 
     m1 = F @ ms
     N1_ = tria(jnp.concatenate((F @ Ls, cholQ), axis=1))
-    Psi_ = jnp.block([[H @ N1_, cholR], [N1_, jnp.zeros((nx, ny))]])
+    Psi_ = jnp.block([[H @ N1_, jnp.zeros((ny, ny))], [N1_, jnp.zeros((nx, ny))]])
     Tria_Psi_ = tria(Psi_)
     Psi11 = Tria_Psi_[:ny, :ny]
     Psi21 = Tria_Psi_[ny : ny + nx, :ny]
