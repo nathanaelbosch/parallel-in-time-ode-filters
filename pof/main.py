bf309f6487046b06ec123c235213a80651ce2049
Nathanael Bosch <nathanael.bosch@uni-tuebingen.de>
2022-05-12 17:38:37 +0300

    Make the "get initial trajectory" function more powerful

--- pof/main.py
+++ pof/main.py
@@ -24,15 +24,32 @@ def discretize_transitions(iwp, times):
     return get_transitions(iwp, steps)
 
 
-def get_constant_initial_trajectory(y0, dy0, order, N, with_dy=True):
-    d = y0.shape[0]
-    if with_dy:
-        _x0 = jnp.concatenate(
+def get_initial_trajectory(y0, f, order, N=None, with_dy=True):
+    d = y0.shape[-1]
+    if len(y0.shape) == 1:
+        assert N is not None
+        # Single initial value => constant initial trajectory
+        if with_dy:
+            dy0 = f(None, y0)
+        else:
+            dy0 = jnp.zeros_like(y0)
+        x0 = jnp.concatenate(
             [y0[:, None], dy0[:, None], jnp.zeros((d, (order - 1)))], axis=1
         )
+        x0 = x0.reshape(1, -1)
+        traj = jnp.repeat(x0, N, axis=0)
     else:
-        _x0 = jnp.concatenate([y0[:, None], jnp.zeros((d, (order)))], axis=1)
-    return jnp.repeat(_x0.reshape(1, -1), N, axis=0)
+        assert N is None
+        N = y0.shape[0]
+        if with_dy:
+            dy0 = jax.vmap(f, in_axes=(None, 0))(None, y0)
+        else:
+            dy0 = jnp.zeros_like(y0)
+        traj = jnp.concatenate(
+            [y0[:, :, None], dy0[:, :, None], jnp.zeros((N, d, (order - 1)))], axis=1
+        )
+        traj = traj.reshape(N, -1)
+    return traj
 
 
 def linearize_observation_model(observation_model, trajectory):
