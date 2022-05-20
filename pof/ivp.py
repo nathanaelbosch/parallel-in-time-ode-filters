7b0e03f5c36edcc504e3841eebafe0f0a50021cd
Nathanael Bosch <nathanael.bosch@uni-tuebingen.de>
2022-05-20 15:28:53 +0300

    Small IVP formatting change

--- pof/ivp.py
+++ pof/ivp.py
@@ -11,12 +11,7 @@ def logistic(t0=0, tmax=10.0, y0=None):
     def f(t, y):
         return 1.0 * y * (1 - y)
 
-    return tornadox.ivp.InitialValueProblem(
-        f=f,
-        t0=t0,
-        tmax=tmax,
-        y0=y0,
-    )
+    return tornadox.ivp.InitialValueProblem(f=f, t0=t0, tmax=tmax, y0=y0)
 
 
 def lotkavolterra(t0=0.0, tmax=7, y0=None, p=None):
@@ -27,11 +22,11 @@ def lotkavolterra(t0=0.0, tmax=7, y0=None, p=None):
     @jax.jit
     def f(_, Y, p=p):
         a, b, c, d = p
-        return jnp.array([a * Y[0] - b * Y[0] * Y[1], -c * Y[1] + d * Y[0] * Y[1]])
+        return jnp.array(
+            [
+                a * Y[0] - b * Y[0] * Y[1],
+                -c * Y[1] + d * Y[0] * Y[1],
+            ]
+        )
 
-    return tornadox.ivp.InitialValueProblem(
-        f=f,
-        t0=t0,
-        tmax=tmax,
-        y0=y0,
-    )
+    return tornadox.ivp.InitialValueProblem(f=f, t0=t0, tmax=tmax, y0=y0)
