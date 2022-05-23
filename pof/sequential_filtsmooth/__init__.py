2a1f5f38d6b3f0a0f387266cdf86cc441e9e91b2
Nathanael Bosch <nathanael.bosch@uni-tuebingen.de>
2022-05-23 14:00:35 +0300

    Add a convenient sequential filtsmooth

--- pof/sequential_filtsmooth/__init__.py
+++ pof/sequential_filtsmooth/__init__.py
@@ -1,2 +1,10 @@
 from .filter import extended_kalman_filter
 from .smoother import smoothing
+
+
+def filtsmooth(x0, linear_transitions, continuous_observation_model):
+    out, nll = extended_kalman_filter(
+        x0, linear_transitions, continuous_observation_model
+    )
+    out = smoothing(linear_transitions, out)
+    return out, nll
