3f323604e1138bf07d65e961decd87b002f04c25
Nathanael Bosch <nathanael.bosch@uni-tuebingen.de>
2022-10-05 17:50:34 +0200

    Update _big parts of_ my code

--- experiments/1_simple_runtime_benchmark/plot_data.py
+++ experiments/1_simple_runtime_benchmark/plot_data.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from cycler import cycler
@@ -19,25 +20,24 @@ plt.rcParams.update(
     }
 )
 
-fig, axes = plt.subplots(1, 2, sharey=True)
-
+fig, axes = plt.subplots(1, 2, sharey=True, sharex=True)
 
 colored = (
-    cycler("color", ["b", "r"])
+    cycler("color", ["r", "b"])
     + cycler("marker", ["*", "^"])
     + cycler("linestyle", ["-", "-"])
 )
-colored2 = (
-    cycler("color", ["g", "y"])
-    + cycler("marker", ["*", "^"])
-    + cycler("linestyle", ["-", "-"])
-)
+# colored2 = (
+#     cycler("color", ["g", "y"])
+#     + cycler("marker", ["*", "^"])
+#     + cycler("linestyle", ["-", "-"])
+# )
+# colored = colored.concat(colored2)
 monochrome = cycler("color", ["gray"]) * (
     cycler("linestyle", [(0, (3, 1, 1, 1)), "--", ":", "-.", (0, (3, 1, 1, 1, 1, 1))])
     + cycler("marker", ["p", "P", "X", "d", "s"])
 )
-# cycle = colored.concat(monochrome)
-cycle = colored.concat(colored2).concat(monochrome)
+cycle = colored.concat(monochrome)
 
 filenames = ("logistic", "lotkavolterra")
 titles = ("Logistic equation", "Lotka-Volterra")
@@ -47,32 +47,44 @@ for i, ax in enumerate(axes):
     ax.set_prop_cycle(cycle)
 
     folder = Path("experiments/1_simple_runtime_benchmark/")
-    print(f"{i}")
-    df = pd.read_csv(folder / f"{filenames[i]}_dev.csv", index_col=0)
+    df = pd.read_csv(folder / f"{filenames[i]}_filter_only.csv", index_col=0)
     x = "N"
-    ax.plot(
-        df[x], df.pEKS, label="parallel EKS", markersize=12, linewidth=4, zorder=101
-    )
     ax.plot(
         df[x], df.sEKS, label="sequential EKS", markersize=10, linewidth=4, zorder=100
     )
     ax.plot(
-        df[x], df.pEKSq, label="parallel EKS QR", markersize=12, linewidth=4, zorder=101
+        df[x], df.pEKS, label="parallel EKS", markersize=12, linewidth=4, zorder=101
     )
-    ax.plot(
-        df[x],
-        df.sEKSq,
-        label="sequential EKS QR",
-        markersize=10,
-        linewidth=4,
-        zorder=100,
-    )
+    # ax.plot(df[x], df.pEKSq, label="parallel EKS QR", markersize=12, linewidth=4, zorder=101)
+    # ax.plot(df[x], df.sEKSq, label="sequential EKS QR", markersize=10, linewidth=4, zorder=100,)
 
     ref_alpha = 1
-    ax.plot(df[x], df.dp5, label="Dopri5 (diffrax)", alpha=ref_alpha)
+    # ax.plot(df[x], df.dp5, label="Dopri5 (diffrax)", alpha=ref_alpha)
     # ax.plot(df[x], df.kv5, label="Kvaerno5 (diffrax)", alpha=ref_alpha)
-    ax.plot(df[x], df.rk45, label="RK45 (scipy)", alpha=ref_alpha)
-    ax.plot(df[x], df.lsoda, label="LSODA (scipy)", alpha=ref_alpha)
+    # ax.plot(df[x], df.rk45, label="RK45 (scipy)", alpha=ref_alpha)
+    # ax.plot(df[x], df.lsoda, label="LSODA (scipy)", alpha=ref_alpha)
+
+    ax.plot(
+        df[x],
+        df[x] / 8000 if i == 0 else df[x] / 5000,
+        label="$\propto$ N",
+        marker="",
+        alpha=0.3,
+        linewidth=10,
+        linestyle="solid",
+        color="r",
+    )
+
+    ax.plot(
+        df[x],
+        np.log(df[x]) / 1300 if i == 0 else np.log(df[x]) / 800,
+        label="$\propto$ log(N)",
+        marker="",
+        alpha=0.3,
+        linewidth=10,
+        linestyle="solid",
+        color="b",
+    )
 
     # if i == 0:
     #     ax.plot(df[x], df.probnumek0, label="EK0 (probnum)", alpha=ref_alpha)
@@ -88,4 +100,4 @@ axes[1].set_xlabel("Number of gridpoints")
 axes[0].set_ylabel("Runtime [s]")
 axes[0].legend()
 
-fig.savefig(folder / f"plot_qr.pdf")
+fig.savefig(folder / f"plot.pdf")
--- scripts/2022-06-16-gif.py
+++ scripts/2022-06-16-gif.py
@@ -16,88 +16,127 @@ from pof.iterators import *
 fs = linear_filtsmooth
 lom = jax.jit(jax.vmap(linearize, in_axes=[None, 0]), static_argnums=(0,))
 
-
-def set_up(ivp, dt, order):
-    ts = jnp.arange(ivp.t0, ivp.tmax + dt, dt)
-
-    iwp = IWP(num_derivatives=order, wiener_process_dimension=ivp.y0.shape[0])
-    dtm = jax.vmap(lambda _: TransitionModel(*preconditioned_discretize(iwp)))(ts[1:])
-    P, PI = nordsieck_preconditioner(iwp, dt)
-
-    E0, E1 = projection_matrix(iwp, 0), projection_matrix(iwp, 1)
-    E0, E1 = E0 @ P, E1 @ P
-    om = NonlinearModel(lambda x: E1 @ x - ivp.f(None, E0 @ x))
-
-    x0 = init.taylor_mode_init(ivp.f, ivp.y0, order)
-    # x0 = uncertain_init(ivp.f, ivp.y0, order, var=1e0)
-    x0 = jax.tree_map(lambda x: PI @ x, x0)
-
-    return {
-        "ts": ts,
-        "dtm": dtm,
-        "om": om,
-        "x0": x0,
-        "E0": E0,
-        "ivp": ivp,
-        "P": P,
-        "PI": PI,
-        "order": order,
-        "iwp": iwp,
-    }
+MAXITER = 1000
 
 
-def ieks_iterator(dtm, om, x0, init_traj, maxiter=250):
-    bar = trange(maxiter)
-    iterator = pof.iterators.ieks_iterator(dtm, om, x0, init_traj)
-    for _, (out, nll, obj) in zip(bar, iterator):
-        yield out, nll, obj
-        bar.set_description(f"[OBJ={obj:.4e} NLL={nll:.4e}]")
+# def set_up(ivp, dt, order):
+#     ts = jnp.arange(ivp.t0, ivp.tmax + dt, dt)
+
+#     iwp = IWP(num_derivatives=order, wiener_process_dimension=ivp.y0.shape[0])
+#     dtm = jax.vmap(lambda _: TransitionModel(*preconditioned_discretize(iwp)))(ts[1:])
+#     P, PI = nordsieck_preconditioner(iwp, dt)
+
+#     E0, E1 = projection_matrix(iwp, 0), projection_matrix(iwp, 1)
+#     E0, E1 = E0 @ P, E1 @ P
+#     om = NonlinearModel(lambda x: E1 @ x - ivp.f(None, E0 @ x))
+
+#     x0 = init.taylor_mode_init(ivp.f, ivp.y0, order)
+#     # x0 = uncertain_init(ivp.f, ivp.y0, order, var=1e0)
+#     x0 = jax.tree_map(lambda x: PI @ x, x0)
+
+#     return {
+#         "ts": ts,
+#         "dtm": dtm,
+#         "om": om,
+#         "x0": x0,
+#         "E0": E0,
+#         "ivp": ivp,
+#         "P": P,
+#         "PI": PI,
+#         "order": order,
+#         "iwp": iwp,
+#     }
 
 
-def qpm_ieks_iterator(dtm, om, x0, init_traj, maxiter=500):
-    bar = trange(maxiter)
-    iterator = pof.iterators.qpm_ieks_iterator(
-        dtm,
-        om,
-        x0,
-        init_traj,
-        reg_start=1e20,
-        reg_final=1e-20,
-        steps=100,
-        # tau_start=1e0,
-        # tau_final=1e-10,
-    )
-    for _, (out, nll, obj, reg) in zip(bar, iterator):
-        yield out, nll, obj
-        bar.set_description(f"[OBJ={obj:.4e} NLL={nll:.4e} reg={reg:.4}]")
+# def ieks_iterator(dtm, om, x0, init_traj, maxiter=MAXITER):
+#     bar = trange(maxiter)
+#     iterator = pof.iterators.ieks_iterator(dtm, om, x0, init_traj)
+#     for _, (out, nll, obj) in zip(bar, iterator):
+#         yield out, nll, obj
+#         bar.set_description(f"[OBJ={obj:.4e} NLL={nll:.4e}]")
 
 
-def _precondition(setup, raw_states):
-    precondition = lambda v: setup["PI"] @ v
-    preconditioned_states = jax.tree_map(jax.vmap(precondition), raw_states)
-    return preconditioned_states
+# def qpm_ieks_iterator(dtm, om, x0, init_traj, maxiter=MAXITER):
+#     bar = trange(maxiter)
+#     iterator = pof.iterators.qpm_ieks_iterator(
+#         dtm,
+#         om,
+#         x0,
+#         init_traj,
+#         reg_start=1e20,
+#         reg_final=1e-20,
+#         steps=100,
+#         # steps=40,
+#         # tau_start=1e0,
+#         # tau_final=1e-10,
+#     )
+#     for _, (out, nll, obj, reg) in zip(bar, iterator):
+#         yield out, nll, obj
+#         bar.set_description(f"[OBJ={obj:.4e} NLL={nll:.4e} reg={reg:.4}]")
 
 
-def prior_init(setup):
-    return init.prior_init(x0=setup["x0"], dtm=setup["dtm"])
+# def lm_ieks_iterator(dtm, om, x0, init_traj, maxiter=250):
+#     bar = trange(maxiter)
+#     iterator = pof.iterators.lm_ieks_iterator(
+#         dtm,
+#         om,
+#         x0,
+#         init_traj,
+#         reg=1e-100,
+#         nu=10,
+#     )
+#     for _, (out, nll, obj, reg) in zip(bar, iterator):
+#         yield out, nll, obj
+#         bar.set_description(f"[OBJ={obj:.4e} NLL={nll:.4e} reg={reg:.4}]")
 
 
-def coarse_solver_init(setup):
-    ivp, order, ts = setup["ivp"], setup["order"], setup["ts"]
-    raw_traj = init.coarse_rk_init(f=ivp.f, y0=ivp.y0, order=order, ts=ts[1:])
-    return _precondition(setup, raw_traj)
+# def _precondition(setup, raw_states):
+#     precondition = lambda v: setup["PI"] @ v
+#     preconditioned_states = jax.tree_map(jax.vmap(precondition), raw_states)
+#     return preconditioned_states
 
 
-ivp = vanderpol()
-dt = 1e-3
+# def prior_init(setup):
+#     states = init.prior_init(
+#         f=setup["ivp"].f, y0=setup["ivp"].y0, order=setup["order"], ts=setup["ts"]
+#     )
+#     return _precondition(setup, states)
+
+
+# def coarse_solver_init(setup):
+#     ivp, order, ts = setup["ivp"], setup["order"], setup["ts"]
+#     raw_traj = init.coarse_rk_init(f=ivp.f, y0=ivp.y0, order=order, ts=ts[1:])
+#     return _precondition(setup, raw_traj)
+
+
+ivp, ylims = vanderpol(tmax=10, stiffness_constant=1e0), (-2.5, 2.5)
+dt = 1e-1
+print(f"Number of steps: {ivp.tmax // dt}")
 order = 3
-setup = set_up(ivp, dt, order=order)
-sol_true = solve_diffrax(ivp.f, ivp.y0, ivp.t_span, atol=1e-20, rtol=1e-20)
-ys_true = jax.vmap(sol_true.evaluate)(setup["ts"])
-init_traj = prior_init(setup)
-ieks_iter = ieks_iterator(setup["dtm"], setup["om"], setup["x0"], init_traj)
+# setup = set_up(ivp, dt, order=order)
+# sol_true = solve_diffrax(ivp.f, ivp.y0, ivp.t_span, max_steps=int(1e6))
+# ys_true = jax.vmap(sol_true.evaluate)(setup["ts"])
+# init_traj = prior_init(setup)
+# init_traj = jax.tree_map(lambda l: l[:-1], init_traj)
+# init_traj = MVNSqrt(
+#     jnp.repeat(_states.mean[:-1, None, ...], 10, axis=1).reshape(-1, 8),
+#     jnp.repeat(_states.chol[:-1, None, ...], 10, axis=1).reshape(-1, 8, 8),
+# )
+# init_traj
+ts = jnp.arange(ivp.t0, ivp.tmax + dt, dt)
+ieks_iter, setup = ieks_iterator(f=ivp.f, y0=ivp.y0, ts=ts, order=order)
+
+# setup["dtm"], setup["om"], setup["x0"], init_traj)
+# ieks_iter = qpm_ieks_iterator(setup["dtm"], setup["om"], setup["x0"], init_traj)
 
 project = lambda states: jnp.dot(setup["E0"], states.mean.T).T
+
+
+@jax.vmap
+def project_cov(cholcov):
+    out_chol = setup["E0"] @ cholcov
+    return out_chol @ out_chol.T
+
 
 from celluloid import Camera
 
@@ -106,14 +145,22 @@ camera = Camera(fig)
 
 for (k, (states, nll, obj)) in enumerate(ieks_iter):
     ys = project(states)
+    covs = project_cov(states.chol)
     d = ys.shape[1]
-    plt.plot(setup["ts"], ys_true, "--k")
-    for i in range(d):
+    plt.plot(setup["ts"], ys_true[:, 0], "--k", linewidth=0.5)
+    for i in range(1):
         plt.plot(setup["ts"], ys[:, i], color=f"C{i}")
-    # plt.ylim(-2, 10)
-    plt.ylim(-5, 5)
+        plt.fill_between(
+            setup["ts"],
+            ys[:, i] - 2 * jnp.sqrt(covs[:, i, i]),
+            ys[:, i] + 2 * jnp.sqrt(covs[:, i, i]),
+            color=f"C{i}",
+        )
+    plt.text(0, ylims[1] - 0.5, f"Iteration {k}")
+    plt.ylim(*ylims)
     camera.snap()
 
-# animation = camera.animate()
-# animation.save("qpm_ieks_vanderpol.mp4")
-animation = camera.animate()
+animation = camera.animate(interval=50)
+filename = f"vdp_{ivp.tmax}_{dt}.gif"
+# animation.save(filename)
+# print(f"Saved to {filename}")
