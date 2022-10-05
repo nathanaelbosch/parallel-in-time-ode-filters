10742cfa37499144c617c510809ef902f9ce4de4
Nathanael Bosch <nathanael.bosch@uni-tuebingen.de>
2022-10-05 17:50:08 +0200

    Add an experiment that compares GPUs!

---
+++ experiments/3_gpu_comparison/collect_data.py
@@ -0,0 +1,146 @@
+import time
+from collections import defaultdict
+import os
+
+import plac
+import jax
+import jax.numpy as jnp
+import numpy as np
+import pandas as pd
+import diffrax
+from scipy.integrate import solve_ivp
+
+from pof.convenience import linearize_observation_model
+from pof.ivp import lotkavolterra, logistic
+from pof.parallel_filtsmooth import linear_filtsmooth
+
+from pof.convenience import set_up_solver, get_initial_trajectory
+from pof.sequential_filtsmooth import filtsmooth as seq_fs
+from pof.utils import _gmul
+
+
+def parallel_eks(f, y0, ts, order):
+    setup = set_up_solver(f=f, y0=y0, ts=ts, order=order)
+
+    dtm = setup["dtm"]
+    om = setup["om"]
+    x0 = setup["x0"]
+    states = get_initial_trajectory(setup, method="constant")
+
+    dom = linearize_observation_model(om, jax.tree_map(lambda l: l[1:], states))
+    out, _, _, _ = linear_filtsmooth(x0, dtm, dom)
+    return out.mean, 0
+
+
+def sequential_eks(f, y0, ts, order):
+    setup = set_up_solver(f=f, y0=y0, ts=ts, order=order)
+
+    dtm = setup["dtm"]
+    om = setup["om"]
+    x0 = setup["x0"]
+
+    states, nll = seq_fs(x0, dtm, om)
+
+    info_dict = {"nll": nll}
+    states = jax.vmap(_gmul, in_axes=[None, 0])(setup["E0"], states)
+
+    return states.mean, 0
+
+
+def solve_diffrax(f, y0, ts, solver):
+    sol = diffrax.diffeqsolve(
+        diffrax.ODETerm(lambda t, y, args: f(t, y)),
+        solver,
+        t0=ts[0],
+        t1=ts[-1],
+        dt0=ts[1] - ts[0],
+        y0=y0,
+        saveat=diffrax.SaveAt(steps=True, t0=True, dense=True),
+        stepsize_controller=diffrax.ConstantStepSize(),
+        max_steps=2 * ts.shape[0],
+        throw=False,
+    )
+    return sol.ys, sol.result
+
+
+def solve_scipy(f, y0, ts, solver):
+    dt = ts[1] - ts[0]
+    tspan = (ts[0], ts[-1])
+    sol = solve_ivp(f, tspan, y0, method=solver, max_step=dt)
+    return sol.y, sol.status
+
+
+def timeit(f, N):
+    times = []
+    for _ in range(N):
+        start = time.time()
+        f()
+        t = time.time() - start
+        times.append(t)
+    return min(times)
+
+
+def benchmark(methods, dts, ivp, N=10):
+    results = defaultdict(lambda: [])
+    for dt in dts:
+        print(f"dt={dt}")
+        results["dt"].append(dt)
+        ts = jnp.arange(ivp.t0, ivp.tmax + dt, dt)
+        for k, v in methods.items():
+            print(k)
+            f = lambda: v(ts)
+            status = f()
+            if status != 0:
+                t = np.nan
+            else:
+                t = timeit(f, N)
+            print(t)
+            results[k].append(t)
+
+    return results
+
+
+def block_and_return_state(f):
+    def f2(*args, **kwargs):
+        a, b = f(*args, **kwargs)
+        a.block_until_ready()
+        return b
+
+    return f2
+
+
+@plac.pos("gpu_name", "GPU Name", choices=["1060", "v100", "2080ti"])
+@plac.opt("dtmin", "Minimum Step Size (negative log2)", type=int)
+def main(gpu_name, dtmin=15):
+
+    dts = 2.0 ** -np.arange(14, 15)
+
+    ivp = logistic()
+    f, y0 = ivp.f, ivp.y0
+
+    peks = jax.jit(lambda ts: parallel_eks(f, y0, ts, order=4))
+    seks = jax.jit(lambda ts: sequential_eks(f, y0, ts, order=4))
+    dp5 = jax.jit(lambda ts: solve_diffrax(f, y0, ts, solver=diffrax.Dopri5()))
+    Kv5 = diffrax.Kvaerno5(diffrax.NewtonNonlinearSolver(rtol=1e-6, atol=1e-9))
+    kv5 = jax.jit(lambda ts: solve_diffrax(f, y0, ts, solver=Kv5))
+    rk45 = lambda ts: solve_scipy(f, y0, ts, "RK45")[1]
+    lsoda = lambda ts: solve_scipy(f, y0, ts, "LSODA")[1]
+    methods = {
+        "pEKS": block_and_return_state(peks),
+        "sEKS": block_and_return_state(seks),
+        "dp5": block_and_return_state(dp5),
+        "kv5": block_and_return_state(kv5),
+    }
+
+    res = benchmark(methods, dts, ivp, N=5)
+
+    df = pd.DataFrame(res)
+    df["N"] = (ivp.tmax - ivp.t0) / df.dt
+
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    filepath = os.path.join(current_dir, f"{gpu_name}_new.csv")
+    df.to_csv(filepath)
+
+
+if __name__ == "__main__":
+    plac.call(main)
---
+++ experiments/3_gpu_comparison/plot_data.py
@@ -0,0 +1,141 @@
+from pathlib import Path
+from collections import defaultdict
+
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import pandas as pd
+from cycler import cycler
+from tueplots import axes, bundles
+from tueplots.constants import markers
+from cuda_cores import CUDA_CORES
+
+plt.rcParams.update(bundles.jmlr2001())
+plt.rcParams.update(axes.lines())
+plt.rcParams.update(
+    {
+        # "lines.linewidth": 1.5,
+        "lines.markeredgecolor": "black",
+        "lines.markeredgewidth": 0.2,
+        # "lines.markersize": 4,
+        "axes.grid": True,
+        "axes.grid.which": "both",
+    }
+)
+colored = (
+    cycler("color", ["r", "b"])
+    + cycler("marker", ["*", "^"])
+    + cycler("linestyle", ["-", "-"])
+)
+monochrome = cycler("color", ["gray"]) * (
+    cycler("linestyle", [(0, (3, 1, 1, 1)), "--", ":", "-.", (0, (3, 1, 1, 1, 1, 1))])
+    + cycler("marker", ["p", "P", "X", "d", "s"])
+)
+cycle = colored.concat(monochrome)
+
+
+# filedir = Path(__file__).parent
+filedir = Path("experiments/3_gpu_comparison")
+gpus = ["1060", "2080ti", "v100"]
+dfs = {gpu: pd.read_csv(filedir / f"{gpu}.csv") for gpu in gpus}
+
+labels = {
+    "pEKS": "parallel EKS",
+    "sEKS": "sequential EKS",
+    "dp5": "Dopri5 (diffrax)",
+    "kv5": "Kvaerno5 (diffrax)",
+}
+gpu_labels = {
+    "1060": "GTX 1060",
+    "2080ti": "RTX 2080 Ti",
+    "v100": "V100",
+}
+
+fig, axes = plt.subplots(1, len(gpus), sharey=True, sharex=True)
+
+x = "N"
+for ax, gpu in zip(axes, gpus):
+    ax.set_prop_cycle(cycle)
+    df = dfs[gpu]
+    for i, m in enumerate(("sEKS", "pEKS")):
+        z = 100 + i
+        ax.plot(df[x], df[m], label=labels[m], markersize=10, linewidth=4, zorder=z)
+
+    ref_alpha = 1
+    for m in ("dp5", "kv5"):
+        ax.plot(df[x], df[m], label=labels[m], alpha=ref_alpha)
+
+    ax.set_xscale("log")
+    ax.set_yscale("log")
+
+axes[0].set_xlabel("Number of gridpoints")
+axes[1].set_xlabel("Number of gridpoints")
+axes[2].set_xlabel("Number of gridpoints")
+axes[0].set_title("GeForce GTX 1060")
+axes[1].set_title("GeForce RTX 2080 Ti")
+axes[2].set_title("Tesla V100")
+axes[0].set_ylabel("Runtime [s]")
+axes[0].legend()
+
+fig.savefig(filedir / "plot.pdf", bbox_inches="tight")
+
+
+DT = 2 ** -13
+rs = defaultdict(lambda: [])
+for gpu in gpus:
+    rs["cores"].append(CUDA_CORES[gpu])
+    df = dfs[gpu]
+    row = df[df.dt == DT]
+    for m in ("sEKS", "pEKS", "dp5", "kv5"):
+        rs[m].append(row[m].values[0])
+df = pd.DataFrame(rs)
+
+
+fig, ax = plt.subplots(1, 1)
+ax.set_prop_cycle(cycle)
+for m in ("sEKS", "pEKS", "dp5", "kv5"):
+    ax.plot(df["cores"], df[m], label=labels[m], markersize=10)
+ax.set_xscale("log")
+ax.set_yscale("log")
+ax.set_xlabel("Number of CUDA cores")
+ax.set_ylabel("Runtime [s]")
+ax.legend()
+fig.savefig(filedir / "plot_cores.pdf", bbox_inches="tight")
+
+
+fig, ax = plt.subplots(1, 1)
+cyc = cycler("color", ["r", "b"]) * cycler("linestyle", ["-", "--", ":"])
+cyc = cycler("color", ["r", "b", "gray", "gray"]) * cycler(
+    "linestyle", ["-", "--", ":"]
+)
+ax.set_prop_cycle(cyc)
+for i, m in enumerate(("sEKS", "pEKS", "dp5", "kv5")):
+    for gpu in gpus:
+        df = dfs[gpu]
+        ax.plot(df[x], df[m], linewidth=2, label=f"{labels[m]} ({gpu_labels[gpu]})")
+ax.set_xscale("log")
+ax.set_yscale("log")
+ax.set_xlabel("Number of gridpoints")
+ax.set_ylabel("Runtime [s]")
+# ax.legend(bbox_to_anchor=(1.05, 0.5), loc="center left")
+
+legend_elements = [
+    matplotlib.patches.Patch(facecolor="r", label="Sequential EKS"),
+    matplotlib.patches.Patch(facecolor="b", label="Parallel EKS"),
+    matplotlib.patches.Patch(facecolor="gray", label="Dopri5 (diffrax)"),
+    matplotlib.patches.Patch(facecolor="gray", label="Kvaerno5 (diffrax)"),
+]
+lg = ax.legend(handles=legend_elements, loc="upper left")
+ax.add_artist(lg)
+legend_elements = [
+    matplotlib.lines.Line2D([0], [0], linestyle="-", color="k", label="GTX 1060"),
+    matplotlib.lines.Line2D([0], [0], linestyle="--", color="k", label="RTX 2080 Ti"),
+    matplotlib.lines.Line2D([0], [0], linestyle=":", color="k", label="V100"),
+]
+ax.legend(handles=legend_elements, loc="lower right")
+# ax.legend(handles=legend_elements, loc="upper left")
+
+for i, gpu in enumerate(gpus):
+    s = ["-", "--", ":"][i]
+    ax.axvline(CUDA_CORES[gpu], 0.0, 1.0, color="black", linewidth=1.0, linestyle=s)
+fig.savefig(filedir / "gpu_comparison.pdf", bbox_inches="tight")
