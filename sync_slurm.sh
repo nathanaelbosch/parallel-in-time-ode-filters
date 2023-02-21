16fd12863d3bf0009ed5cdc93c2974a13f55a4e8
Nathanael Bosch <nathanael.bosch@uni-tuebingen.de>
2023-02-21 10:55:29 +0100

    Remove more folders from the syncing

--- sync_slurm.sh
+++ sync_slurm.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 rsync -rv ./ slurm:/mnt/qb/work/hennig/nbosch12/parallel-ode-filters \
-      --exclude={".venv/","oldstuff/",".tox","experiments/old.2_init_comparison","__pycache__"} \
+      --exclude={".venv/","oldstuff/",".tox","experiments/old.*","__pycache__"} \
       --exclude={"*.csv","*.pdf","*.png"}
