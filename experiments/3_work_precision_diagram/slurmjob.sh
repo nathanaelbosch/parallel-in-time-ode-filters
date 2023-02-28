1099074013233859a003ce5507ef93b10b2195e0
Nathanael Bosch <nathanael.bosch@uni-tuebingen.de>
2023-02-28 10:37:00 +0100

    Update job submission script

--- experiments/3_work_precision_diagram/slurmjob.sh
+++ experiments/3_work_precision_diagram/slurmjob.sh
@@ -2,7 +2,7 @@
 #SBATCH --ntasks=1                # Number of tasks (see below)
 #SBATCH --cpus-per-task=1         # Number of CPU cores per task
 #SBATCH --nodes=1                 # Ensure that all cores are on one machine
-#SBATCH --time=0-24:00            # Runtime in D-HH:MM
+#SBATCH --time=3-00:00            # Runtime in D-HH:MM
 # #SBATCH --partition=gpu-v100      # Partition to submit to
 # #SBATCH --gres=gpu:1              # optionally type and number of gpus
 #SBATCH --mem=32G                 # Memory pool for all cores (see also --mem-per-cpu)
