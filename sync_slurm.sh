7b72471e8b97b90107765d5a0a1c55aac9218465
Nathanael Bosch <nathanael.bosch@uni-tuebingen.de>
2023-09-28 17:09:30 +0200

    Remove more things that don't need to be part of the repo

--- sync_slurm.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-rsync -r ./ slurm:/mnt/qb/work/hennig/nbosch12/parallel-ode-filters \
-      --exclude={".venv/","oldstuff/",".tox","experiments/old.*","__pycache__","notebooks/.ipynb_checkpoints/",".git/",".pytest_cache"} \
-      --exclude={"*.csv","*.pdf","*.png"}
-
-rsync -r ./ slurm-r2:/mnt/lustre/hennig/nbosch12/parallel-ode-filters \
-      --exclude={".venv/","oldstuff/",".tox","experiments/old.*","__pycache__","notebooks/.ipynb_checkpoints/",".git/",".pytest_cache"} \
-      --exclude={"*.csv","*.pdf","*.png"}
--- sync_wsi.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-rsync -r ./ wsi:parallel-ode-filters \
-      --exclude={".venv/","oldstuff/",".tox","experiments/old.*","__pycache__","notebooks/.ipynb_checkpoints/",".git/",".pytest_cache"} \
-      --exclude={"*.csv","*.pdf","*.png"}
